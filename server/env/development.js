6437ea28ce3235809ddae14ea2af42f4586a9620
Dane Tomseth <danetomseth@gmail.com>
2016-08-16 14:56:56 -0700

    config vars

--- server/env/development.js
+++ server/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
   //"DATABASE_URI": "mongodb://localhost:27017/blink",
-  "DATABASE_URI": "mongodb://blinkdb:blink1026@ds161295.mlab.com:61295/heroku_tt3psr38",
+  "DATABASE_URI": "mongodb://blinkdatabase:blink1026@ds161255.mlab.com:61255/heroku_n96s1l3g",
   "SESSION_SECRET": "Gabe is my real dad",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
