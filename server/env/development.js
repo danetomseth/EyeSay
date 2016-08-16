33abf20c4617d9da31d1b562ba49b4770db0cf4d
Dane Tomseth <danetomseth@gmail.com>
2016-08-16 14:17:25 -0700

    mLab

--- server/env/development.js
+++ server/env/development.js
@@ -1,5 +1,6 @@
 module.exports = {
-  "DATABASE_URI": "mongodb://localhost:27017/blink",
+  //"DATABASE_URI": "mongodb://localhost:27017/blink",
+  "DATABASE_URI": "mongodb://blinkapp:blink1026@ds161295.mlab.com:61295/heroku_tt3psr38",
   "SESSION_SECRET": "Gabe is my real dad",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
