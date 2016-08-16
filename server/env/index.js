f26f495c7b0d9e310088ac148b70094c811283a7
Dane Tomseth <danetomseth@gmail.com>
2016-08-16 14:37:55 -0700

    env

--- server/env/index.js
+++ server/env/index.js
@@ -3,8 +3,9 @@ var devConfigPath = path.join(__dirname, './development.js');
 var productionConfigPath = path.join(__dirname, './production.js');
 
 if (process.env.NODE_ENV === 'production') {
-
+	console.log('Production Env');
     module.exports = require(productionConfigPath);
 } else {
+	console.log("Dev Env");
     module.exports = require(devConfigPath);
 }
