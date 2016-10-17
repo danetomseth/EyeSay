fe30a5598d8b2c5ff9abc71f7a2b803ef0e44395
danetomseth <danetomseth@gmail.com>
2016-10-17 15:08:00 -0400

    changes made on plane

--- browser/js/common/factories/tracking.factory.js
+++ browser/js/common/factories/tracking.factory.js
@@ -1,6 +1,6 @@
 'use strict';
 
-core.factory('TrackingFactory', function($rootScope, DialogFactory) {
+core.factory('TrackingFactory', function($rootScope, DialogFactory, TimerFactory) {
     let canvas;
     let context;
     let tracker;
@@ -18,31 +18,31 @@ core.factory('TrackingFactory', function($rootScope, DialogFactory) {
         canvas = canvasElem;
         context = canvas.getContext("2d");
         //helps remove the error when tracker first loads
+        // Avoids cannot get response model on point XX
 
         setTimeout(function() {
             tracker.setResponseMode("blend", ["raw", "sobel"]);
-            if (boundingBox) {
-                console.log('tracking, bounding box');
-                tracker.start(video, boundingBox);
-            } else {
-                console.log("tracking, no box");
-                tracker.start(video);
-            }
-            $rootScope.$broadcast("trackerInitialized")
+            // if (boundingBox) {
+            //     tracker.start(video, boundingBox);
+            // } else {
+            //     tracker.start(video);
+            // }
+            boundingBox ? tracker.start(video, boundingBox) : tracker.start(video);
+            TimerFactor.start();
         }, 2000);
 
     };
 
     trackObj.startSidebar = () => {
-        var containerWidth = angular.element(document.getElementById('sidebar-webcam-container'))[0].clientWidth
-        var boundingBox = document.getElementById("canvas-overlay");
-        var ctx = boundingBox.getContext("2d");
-        var video = document.getElementById('sidebar-webcam');
-        var canvas = document.getElementById("sidebar-canvas");
-        var middleX = containerWidth - (containerWidth / 4);
-        var middleY = (containerWidth * .75) - ((containerWidth / 3.1));
-        var canvasWidth = (containerWidth / 4) * 2;
-        var canvasHeight = ((containerWidth / 3) * .75) * 2.5;
+        let containerWidth = angular.element(document.getElementById('sidebar-webcam-container'))[0].clientWidth
+        let boundingBox = document.getElementById("canvas-overlay");
+        let ctx = boundingBox.getContext("2d");
+        let video = document.getElementById('sidebar-webcam');
+        let canvas = document.getElementById("sidebar-canvas");
+        let middleX = containerWidth - (containerWidth / 4);
+        let middleY = (containerWidth * .75) - ((containerWidth / 3.1));
+        let canvasWidth = (containerWidth / 4) * 2;
+        let canvasHeight = ((containerWidth / 3) * .75) * 2.5;
 
         ctx.strokeStyle = "rgba(130,255,50, 0.5)";
         ctx.strokeRect(middleX, middleY, canvasWidth, canvasHeight);
